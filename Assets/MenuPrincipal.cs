fef2a73666149ddaf94de1f8e003e2d42a7692e4
LucioArce <luchopelle8@gmail.com>
2019-11-19 01:41:50 -0300

    Menu principal

---
+++ Assets/MenuPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jugar()
+        {
+            SceneManager.LoadScene(1);
+        }
+}
