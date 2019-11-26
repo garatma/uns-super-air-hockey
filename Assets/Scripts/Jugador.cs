cf94627a6dddc7148830fe042fb6705faf279d34
Luciano Vidili <lucianovidili@gmail.com>
2019-11-26 16:29:26 -0300

    jugador 2 moviÃndose al revÃs

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -5,32 +5,77 @@ using Mirror;
 
 public class Jugador : NetworkBehaviour
 {
-	public GameObject area;
-	private float anchoArea, altoArea;
-	private float anchoVentana, altoVentana;
+    public GameObject area;
+    private float anchoArea, altoArea;
+    private float anchoVentana, altoVentana;
 
-	void Start()
-	{
-		anchoArea = area.transform.localScale.x * 1.0f;
-		altoArea = area.transform.localScale.z * 1.0f;
+    public int invertirMovJugador2 = 1;
+    private bool esPrimerUpdate = true;
+    private Vector3 posicionInicial;
 
-		anchoVentana = Screen.width;
-		altoVentana = Screen.height;
-	}
+    void Start()
+    {
+        anchoArea = area.transform.localScale.x * 1.0f;
+        altoArea = area.transform.localScale.z * 1.0f;
 
-	void FixedUpdate()
-	{
-		if (hasAuthority)
-		{
-			Vector3 posicionActual = Vector3.one;
-			posicionActual.x = (anchoArea/anchoVentana) * Input.mousePosition.x;
-			posicionActual.z = (altoArea/altoVentana) * Input.mousePosition.y;
+        anchoVentana = Screen.width;
+        altoVentana = Screen.height;
+    }
 
-			posicionActual.x += area.transform.position.x - (anchoArea/2.0f);
-			posicionActual.z += area.transform.position.z - (altoArea/2.0f);
+    void FixedUpdate()
+    {
+        if (hasAuthority)
+        {
+            Vector3 posicionActual = Vector3.one;
+            Vector3 posicionFinal = Vector3.one;
+            Vector3 difPosicion = Vector3.one;
+            posicionActual.x = (anchoArea / anchoVentana) * Input.mousePosition.x;
+            posicionActual.z = (altoArea / altoVentana) * Input.mousePosition.y;
+            posicionActual.x += area.transform.position.x - (anchoArea / 2.0f);
+            posicionActual.z += area.transform.position.z - (altoArea / 2.0f);
 
-			posicionActual.y = 0.32f;
-			transform.position = posicionActual;
-		}
-	}
+            posicionActual.y = 0.32f;
+            //posicionActual.y = transform.position.y;
+
+            if (esPrimerUpdate)
+            {
+                posicionInicial = posicionActual;
+                esPrimerUpdate = false;
+            }
+            else
+            {
+                if (invertirMovJugador2 == -1)
+                {
+                    posicionActual.x = - posicionActual.x;
+                    posicionActual.z = -posicionActual.z;
+                    posicionFinal = posicionActual;
+
+                    //difPosicion = posicionActual - posicionInicial;
+                    //posicionFinal = posicionInicial - difPosicion;
+                    //posicionFinal.x = invertirMovJugador2 * posicionActual.x;
+                    //posicionFinal.z = invertirMovJugador2 * posicionActual.z;
+                    //posicionFinal.y = 0.32f;
+                }
+                else
+                    posicionFinal = posicionActual;
+
+                transform.position = posicionFinal;
+            }
+
+            //transform.position = posicionActual;
+
+            //posicionActual.x = invertirMovJugador2 * posicionActual.x;
+            //posicionActual.z = invertirMovJugador2 * posicionActual.z;
+
+            // Chequea si cambió su posición, es decir, si se movió el mouse.
+            // Chequeo necesario porque si no, ni bien Respawnea el jugador2, se invierte el valor de z y
+            // reaparece en el campo contrario.
+            //if ((posicionActual.x - transform.position.x >= 0.01f) | (posicionActual.z - transform.position.z >= 0.01f))  // posicionActual.y == transform.position.y == 0.32f SIEMPRE!
+            //    transform.position = posicionActual;
+            //Debug.Log("X");
+            //Debug.Log(transform.position.x);
+            //Debug.Log("Z");
+            //Debug.Log(transform.position.z);
+        }
+    }
 }
